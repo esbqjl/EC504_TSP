68f0af04f5c59b36ec23635a086718ca216f036d
yezhou2000 <113222469+yezhou2000@users.noreply.github.com>
2022-12-12 00:15:51 -0500

    main error fixed

--- src/main.cpp
+++ src/main.cpp
@@ -20,15 +20,26 @@ int MAX_CITIES = 50000;
 vector<int> id;
 vector<pair<double, double> > locate;
 
+typedef struct Node
+{
+    int id;
+    double x;
+    double y;
+
+} Node;
+
+
 int main(int argc, char* argv[])
 {
     string full_file_path(argv[1]);
     string file_name = full_file_path.substr(full_file_path.find_last_of("/") + 1, full_file_path.back());
     string file_name_no_ext = file_name.substr(0, file_name.find_first_of("."));
     string file_path = full_file_path.substr(0, full_file_path.find_last_of("/") + 1);
-    
+
     cout << "file_path: " << file_path << endl;
     cout << "file name: " << file_name << endl;
+
+
 
     string country_id = file_name.substr(0, 2);
     int NUM_CITIES = std::stoi(file_name.substr(2, file_name.find(".")));
@@ -48,6 +59,40 @@ int main(int argc, char* argv[])
         cout << "[ERROR] Too many cities to directly store distances" << endl;
         return -1;
     }
+
+    std::ifstream infile;
+
+    infile.open(argv[1]);
+    if (!infile)
+    {
+        cout << "Error opening file " <<endl;
+        return -1;
+    }
+
+
+    string read_line;
+
+    for (int i = 0; i < START_ROW; i++)
+    {
+        std::getline(infile, read_line);
+    }
+
+    for (int i = START_ROW; i < START_ROW + NUM_CITIES; i++)
+    {
+        std::getline(infile, read_line);
+
+        size_t first_space = read_line.find_first_of(" ");
+        size_t second_space = read_line.find_last_of(" ");
+
+        node_list[i - START_ROW].id = std::stoi(read_line.substr(0, first_space));
+        node_list[i - START_ROW].x = std::stof(read_line.substr(first_space, second_space));
+        node_list[i - START_ROW].y = std::stof(read_line.substr(second_space, string::npos));
+
+        if (DEBUG_FLAG)
+            cout << "Node " << node_list[i - START_ROW].id << ", x = " << node_list[i - START_ROW].x << ", y = " << node_list[i - START_ROW].y << endl;
+    }
+
+    infile.close();
     double* distance_sq_matrix = new double[NUM_CITIES * NUM_CITIES];
 
     for (int i = 0; i < NUM_CITIES; i++)
@@ -65,56 +110,35 @@ int main(int argc, char* argv[])
         }
     }
 
-    string full_file_path2(argv[2]);
-    string file_name2 = full_file_path2.substr(full_file_path2.find_last_of("/") + 1, full_file_path2.back());
-    string file_name_no_ext2 = file_name2.substr(0, file_name2.find_first_of("."));
-    string file_path2 = full_file_path2.substr(0, full_file_path2.find_last_of("/") + 1);
-
-    cout << "file_path: " << file_path2 << endl;
-    cout << "file name: " << file_name2 << endl;
-
-    string country_id2 = file_name2.substr(0, 2);
-    int NUM_CITIES2 = std::stoi(file_name2.substr(2, file_name2.find(".")));
-
-    cout << "Country ID: " << country_id2 << ", Number of cities = " << NUM_CITIES2 << endl;
-
-    Node* node_list2 = new Node[NUM_CITIES2];
-
-
-    double* distance_sq_matrix2 = new double[NUM_CITIES2 * NUM_CITIES2];
-
-    for (int i = 0; i < NUM_CITIES2; i++)
+    for (int i = 0; i < 10; i++)
     {
-        if (i % (NUM_CITIES2/10) == 0)
+        for (int j = 0; j < 10; j++)
         {
-            cout << "Computing distances for node " << i << endl;
+            cout << std::setprecision(2) << std::setw(8) << distance_sq_matrix[NUM_CITIES * i + j] << " ";
         }
-
-        for (int j = i + 1; j < NUM_CITIES2; j++)
-        {
-            double dij_sq2 = (node_list2[i].x - node_list2[j].x) * (node_list2[i].x - node_list2[j].x) + (node_list2[i].y - node_list2[j].y) * (node_list2[i].y - node_list2[j].y);
-            distance_sq_matrix2[NUM_CITIES2 * i + j] = dij_sq2;
-            distance_sq_matrix2[NUM_CITIES2 * j + i] = dij_sq2;
-        }
+        cout << endl;
     }
+    
+
+
 
     /* ALGORITHM 1: DYNAMIC PROGRAMMING */
 
 
     vector < vector<float> > c;
-    for (int i = 0; i != NUM_CITIES2; ++i) {
+    for (int i = 0; i != NUM_CITIES; ++i) {
         vector<float> tmp;
-        for (int j = 0; j != NUM_CITIES2; ++j) {
-            float dij_sq2 = (node_list2[i].x - node_list2[j].x) * (node_list2[i].x - node_list2[j].x) +
-                           (node_list2[i].y - node_list2[j].y) * (node_list2[i].y - node_list2[j].y);
+        for (int j = 0; j != NUM_CITIES; ++j) {
+            float dij_sq = (node_list[i].x - node_list[j].x) * (node_list[i].x - node_list[j].x) +
+                            (node_list[i].y - node_list[j].y) * (node_list[i].y - node_list[j].y);
 
-            tmp.push_back(dij_sq2);
+            tmp.push_back(dij_sq);
         }
 
         c.push_back(tmp);
     }
 
-    dp(c,NUM_CITIES2);
+    dp(c,NUM_CITIES);
 
 
     // -------------------------------------------------------------------------
@@ -133,19 +157,19 @@ int main(int argc, char* argv[])
 
     // SIMULATED ANNEALING PARAMETERS - STRESS TEST PARAMETERS
     // Run with these parameters is you want better results
-    //double TMAX = 10*sqrt(NUM_CITIES);
-    //double alpha = 0.95;
-    //int STEPS = ceil(100 * log(NUM_CITIES));
-    //int ATTEMPTS = 1000 * NUM_CITIES;
-    //int CHANGES = 100 * NUM_CITIES;
-    //int DEBUG_FREQ = 10;
-    //int NUM_EXPERIMENTS = 30;
+    double TMAX = 10*sqrt(NUM_CITIES);
+    double alpha = 0.95;
+    int STEPS = ceil(100 * log(NUM_CITIES));
+    int ATTEMPTS = 1000 * NUM_CITIES;
+    int CHANGES = 100 * NUM_CITIES;
+    int DEBUG_FREQ = 10;
+    int NUM_EXPERIMENTS = 30;
 
     string write_file = file_path + "/results/" + file_name_no_ext + "_tours.csv";
     string log_file = file_path + "/results/" + file_name_no_ext + "_log.csv";
-    
+
     // Run NUM_EXPERIMENTS on the list of NUM_CITIES cities using the above parameters; write final path and run-time
-    // data to directory.  This might take a while... I recommend running on datasets no larger than the ei8246.tsp 
+    // data to directory.  This might take a while... I recommend running on datasets no larger than the ei8246.tsp
     // dataset...it also dumps a LOT of data into a CSV, I DO NOT RECOMMEND OPENING THE CSV, unless it is the wi29.tsp
     // dataset.  MATLAB scripts have been provided for visualizing the results.
     run_suite(node_list, NUM_CITIES, NUM_EXPERIMENTS, TMAX, alpha, STEPS, ATTEMPTS, CHANGES, DEBUG_FREQ, write_file);
@@ -174,22 +198,52 @@ int main(int argc, char* argv[])
     // -------------------------------------------------------------------------
 
     /* ALGORTIHM 4: kNN */
+    int* completed = new int[NUM_CITIES];
+    for(int i = 0 ; i < NUM_CITIES; i++){
+        completed[i] = 0;
+    }
+
+    std::ofstream outfile("result.txt");
+    int place = 0;
+    int count = 0;
+
+    outfile << node_list[place].id << endl;
+    completed[place] = 1;
+    count ++;
+    while(count < NUM_CITIES) {
+        double min = DBL_MAX;
+        int temp;
+        for (int i = 0; i < NUM_CITIES; i++) {
+            if (i != place && completed[i] == 0 && distance_sq_matrix[place * NUM_CITIES + i] < min) {
+                min = distance_sq_matrix[place * NUM_CITIES + i];
+                temp = i;
+            }
+        }
+        place = temp;
+        outfile << node_list[place].id << endl;
+        completed[place] = 1;
+        count ++;
+
+    }
+    outfile.close();
+
+    delete[] completed;
 
     // -------------------------------------------------------------------------
 
     /* ALGORITHM 5: CHRISTOFIDES */
-  
-    
-//    Christofield C(distance_sq_matrix, NUM_CITIES);
-//    C.findEulerGraph();
-//    C.makeHamiltonian();
-//    C.print();
-//    ofstream myfile;
-//    myfile.open ("result.tsp");
-//    for (int i=0;i<C.paths.size()-1;i++)
-//        myfile << C.paths[i]<<endl;
-//    myfile.close();
-    
+
+
+    Christofield C(distance_sq_matrix, NUM_CITIES);
+    C.findEulerGraph();
+    C.makeHamiltonian();
+    C.print();
+    ofstream myfile;
+    myfile.open ("result.tsp");
+    for (int i=0;i<C.paths.size()-1;i++)
+        myfile << C.paths[i]<<endl;
+    myfile.close();
+
     // -------------------------------------------------------------------------
 
     delete node_list;
@@ -197,5 +251,5 @@ int main(int argc, char* argv[])
 
 
     return 0;
-    
+
 }
